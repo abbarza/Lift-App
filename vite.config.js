3cc90cae6646a743c7ba4dacd9673445a4be42f8
abbarza <abbarza@gmail.com>
2026-02-27 17:58:44 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/lift-app/',
+  base: '/Lift-App/', // This must match your repo name exactly
 })
-export default {
-  base: '/Lift-App/', // This MUST match your repository name
-  // ... rest of your config
-}
